f32f157ef59dd99e8c8ec665c680d0aaae9f5905
Isabela <isabelabianca08@gmail.com>
2024-06-30 18:21:28 -0300

    Feito criacao dos projetos e adicionado injecao de dependencia.

---
+++ Infraestrutura/RepositorioServico.cs
@@ -0,0 +1,32 @@
+﻿using Dominio;
+
+namespace Infraestrutura
+{
+    public class RepositorioServico : IRepositorioServico
+    {
+        public Servico Adicionar(Servico servico)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Servico Atualizar(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Servico ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Servico ObterPorQuery(ServicoDTO filtro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Servico> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Servico.API/Controllers/ServicoController.cs
@@ -0,0 +1,16 @@
+﻿using Infraestrutura;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Servico.API.Controllers
+{
+    [ApiController]
+    public class ServicoController : ControllerBase
+    {
+       private readonly IRepositorioServico _repositorioServico;
+       public ServicoController(IRepositorioServico repositorioServico) {
+            _repositorioServico = repositorioServico;
+        }
+
+
+    }
+}
